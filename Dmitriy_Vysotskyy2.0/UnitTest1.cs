72a2272f0c8b775b0ce3e47a983826fc19e9f521
Dmitriy <dimashkavysotskyy@gmail.com>
2023-05-13 18:50:02 +0300

    init commit

---
+++ Dmitriy_Vysotskyy2.0/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Dmitriy_Vysotskyy2._0;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
